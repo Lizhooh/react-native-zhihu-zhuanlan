4c2bfa4cf519c0877198159379affbbb8ec60385
Lizhooh <960316408@qq.com>
2017-04-08 17:52:42 +0800

    update

--- App/main/api.js
+++
@@ -1,8 +0,0 @@
-
-export const recommPosts = (limit, seed) => {
-    const url = `https://zhuanlan.zhihu.com/api/recommendations/posts?limit=${limit}&seed=${seed}`;
-    return fetch(url).then(res => res.json()).then(result => {
-        return result.map(i => ({ ...i, key: i.id }));
-    });
-}
-
